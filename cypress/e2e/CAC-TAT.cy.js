5be604d92c34dfe7262a23a28d61c5144087e5bd
Murilo Pereira <murilops01.ofi@gmail.com>
2026-02-10 15:10:42 -0300

    translate, select and error check

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -2,11 +2,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   beforeEach(() => {
     cy.visit('./src/index.html')
   })
-  it('verifica o título da aplicação', () => {
+  it('Check the application title', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 
-  it.only('preeche campos obrigatorios', () =>{
+  it('Fill in the required fields', () =>{
     cy.get('#firstName').type('murilo')
     cy.get('#lastName').type('pereira')
     cy.get('#email').type('pereira@gmail.com')
@@ -14,5 +14,59 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
+  })
+
+    it('blog select', () =>{
+    cy.get('#firstName').type('murilo')
+    cy.get('#lastName').type('pereira')
+    cy.get('#email').type('pereira@gmail.com')
+    cy.get('#open-text-area').type('algo')
+    cy.get('#product').select('Blog')
+
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
+  it('blog select', () =>{
+    cy.get('#firstName').type('murilo')
+    cy.get('#lastName').type('pereira')
+    cy.get('#email').type('pereira@gmail.com')
+    cy.get('#open-text-area').type('algo')
+    cy.get('#product').select('Cursos')
+
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
+  it('blog select', () =>{
+    cy.get('#firstName').type('murilo')
+    cy.get('#lastName').type('pereira')
+    cy.get('#email').type('pereira@gmail.com')
+    cy.get('#open-text-area').type('algo')
+    cy.get('#product').select('Mentoria')
+
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
+  it('blog select', () =>{
+    cy.get('#firstName').type('murilo')
+    cy.get('#lastName').type('pereira')
+    cy.get('#email').type('pereira@gmail.com')
+    cy.get('#open-text-area').type('algo')
+    cy.get('#product').select('YouTube')
+
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
+  it('error check', () => {
+    cy.get('.button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
   })
 })
